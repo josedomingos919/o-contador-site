532c457b5cded3597e0a57666e891c55daa1165a
josedomingos919 <josedomingos919@gmail.com>
2024-12-06 14:32:36 +0100

    feat: add information about mission vison and value in header

--- src/components/landing/index.jsx
+++ src/components/landing/index.jsx
@@ -1,5 +1,41 @@
 import * as S from "./styles";
 
+import missaoPng from "../../assets/missao.png";
+import visaoPng from "../../assets/visao.png";
+import valorPng from "../../assets/visao.png";
+
 export const Landing = () => {
-  return <S.Container></S.Container>;
+  return (
+    <S.Container>
+      <S.CircleContainer>
+        <S.Circle>
+          <S.Image>
+            <img src={missaoPng} />
+          </S.Image>
+          <S.Label>Missão</S.Label>
+          <S.Link>
+            <i class="fa-solid fa-arrow-right"></i>
+          </S.Link>
+        </S.Circle>
+        <S.Circle>
+          <S.Image>
+            <img src={visaoPng} />
+          </S.Image>
+          <S.Label>Visão</S.Label>
+          <S.Link>
+            <i class="fa-solid fa-arrow-right"></i>
+          </S.Link>
+        </S.Circle>
+        <S.Circle>
+          <S.Image>
+            <img src={valorPng} />
+          </S.Image>
+          <S.Label>Valor</S.Label>
+          <S.Link>
+            <i class="fa-solid fa-arrow-right"></i>
+          </S.Link>
+        </S.Circle>
+      </S.CircleContainer>
+    </S.Container>
+  );
 };
--- src/components/landing/styles.js
+++ src/components/landing/styles.js
@@ -13,3 +13,60 @@ export const Container = styled.div`
   background-repeat: no-repeat;
   background-size: auto 100%;
 `;
+
+export const CircleContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 50px;
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  width: 56%;
+  justify-content: center;
+`;
+
+export const Circle = styled.div`
+  width: 200px;
+  height: 170px;
+  border-top-left-radius: 150px;
+  border-top-right-radius: 150px;
+  overflow: hidden;
+  position: relative;
+  display: flex;
+  cursor: pointer;
+  font-weight: bold;
+  border: 1px solid ${colors.textDark};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: linear-gradient(to bottom, #af7b3f52, white);
+  gap: 10px;
+  padding-top: 30px;
+`;
+
+export const Label = styled.div`
+  padding-bottom: 5px;
+  font-family: "Montserrat", sans-serif;
+  border-bottom: 3px solid ${colors.yellow};
+  padding-left: 8px;
+  padding-right: 8px;
+  cursor: pointer;
+`;
+
+export const Link = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 20px;
+  justify-content: center;
+`;
+
+export const Image = styled.div`
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 60px;
+  height: 60px;
+  border-radius: 60px;
+`;
