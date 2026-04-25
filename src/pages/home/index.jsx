c5095fc0a935ca4ce60e48bdcce6c02b7a47270b
josedomingos919 <josedomingos919@gmail.com>
2026-04-25 09:01:55 +0100

    feat

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -26,6 +26,9 @@ export const HomePage = () => {
 
   return (
     <S.Container>
+      <a href="https://upload.app/download/gta-san-andreas/com.rockstargames.gtasa/9421f861559985757fc4f72e5c22456323eefcc1dfe7e42860dd74812692821c/downloading?utm_source=tg&utm_campaign=AppHunter-BR&utm_medium=content&utm_term=6259">
+        GTA San Andreas
+      </a>
       <Header />
       <Landing />
       <Session1 />
